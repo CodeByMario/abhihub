2c7de45f541e6a7c91c0b2a6180cc59f750f4228
Abhijeet Shende <abhijeetshende4053@gmail.com>
2026-08-23 00:41:29 +0530

    Patch all collections.abc attributes for compatibility

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,6 +1,7 @@
 import collections
 import collections.abc
 
-# Python 3.10+ compatibility patch for gevent-websocket
-collections.MutableMapping = collections.abc.MutableMapping
-collections.Mapping = collections.abc.Mapping
+# Python 3.10+ compatibility patch for gevent-websocket and legacy libraries
+for name in dir(collections.abc):
+    if not name.startswith('_'):
+        setattr(collections, name, getattr(collections.abc, name))
